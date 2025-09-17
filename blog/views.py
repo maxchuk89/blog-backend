0425d997a848df1cfa6662029b4dca0d6f0c94d8
maxchuk89 <maxchuk89@gmail.com>
2025-09-17 20:02:32 +0300

    подключил карту на странице контактов

--- blog/views.py
+++ blog/views.py
@@ -71,12 +71,12 @@ def contact(request):
     """
     Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
     """
-    # позже здесь будет код для статистики заходов на эту страницу
-    # и для записи фидбека
     folium_map = folium.Map(location=COMPANY_COORDINATES, zoom_start=12)
     folium.Marker(
         COMPANY_COORDINATES,
         tooltip="Мы здесь",
     ).add_to(folium_map)
     html_map = folium_map._repr_html_()
-    return render(request, 'contact.html', {"html_map": html_map})
+
+    # ВАЖНО: рендерим тот шаблон, что у нас есть, и под тем ключом, который он ждёт.
+    return render(request, 'contacts.html', {"map": html_map})
