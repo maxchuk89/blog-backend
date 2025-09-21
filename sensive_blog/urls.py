6ae7aac352fe1e708d10118418752e3ffe157363
maxchuk89 <maxchuk89@gmail.com>
2025-09-21 15:03:50 +0300

    Правки-2

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -1,20 +1,27 @@
+# sensive_blog/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from blog import views
+# 👇 эти два импорта нужны для /favicon.ico
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('post/<slug:slug>', views.post_detail, name='post_detail'),
-    path('contact/', views.contact, name='contact'),
+
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('post/<slug:slug>', views.post_detail, name='post_detail'),
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=staticfiles_storage.url('img/favicon.png'), permanent=True),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
